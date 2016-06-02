e8e8957547bdef4b447aa62750fc54ebfc8837d8
David Ainslie <dainslie@gmail.com>
2016-06-02 18:13:46 +0100

    Bloody regex

--- src/main/scala/uk/gov/homeoffice/amazon/sqs/SQSClient.scala
+++ src/main/scala/uk/gov/homeoffice/amazon/sqs/SQSClient.scala
@@ -5,9 +5,10 @@ import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.services.sqs.AmazonSQSClient
 
 class SQSClient(val sqsHost: URL, credentials: AWSCredentials) extends AmazonSQSClient(credentials) {
-  val Host = """^http[s]?:\/?\/?([^:\/\s]+)\/?\w*\/?""".r
+  val Host = """^(http[s]?:\/?\/?[^:\/\s]+):?(\d*).*""".r
 
   sqsHost.toString match {
-    case Host(h) => setEndpoint(h)
+    case Host(h, p) if p == "" => setEndpoint(h)
+    case Host(h, p) => setEndpoint(s"$h:$p")
   }
 }

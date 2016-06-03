97bf2baff5990aac69d13657f6ff0c19407498b9
David Ainslie <dainslie@gmail.com>
2016-06-03 12:22:10 +0100

    Host parsing for endpoint - needs reworking such as better configuration.

--- src/main/scala/uk/gov/homeoffice/amazon/sqs/SQSClient.scala
+++ src/main/scala/uk/gov/homeoffice/amazon/sqs/SQSClient.scala
@@ -3,12 +3,22 @@ package uk.gov.homeoffice.amazon.sqs
 import java.net.URL
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.services.sqs.AmazonSQSClient
+import grizzled.slf4j.Logging
 
-class SQSClient(val sqsHost: URL, credentials: AWSCredentials) extends AmazonSQSClient(credentials) {
-  val Host = """^(http[s]?:\/?\/?[^:\/\s]+):?(\d*).*""".r
+class SQSClient(val sqsHost: URL, credentials: AWSCredentials) extends AmazonSQSClient(credentials) with Logging {
+  val Host = """^(http[s]?):\/?\/?([^:\/\s]+):?(\d*).*""".r
 
   sqsHost.toString match {
-    case Host(h, p) => setEndpoint(s"$h:$p")
-    case Host(h) => setEndpoint(h)
+    case Host(protocol, "localhost", port) =>
+      info(s"Configuring endpoint as $protocol://localhost:$port")
+      setEndpoint(s"$protocol://localhost:$port")
+
+    case Host(protocol, host, port) if port == "" =>
+      info(s"Configuring endpoint as $host")
+      setEndpoint(s"$host")
+
+    case Host(protocol, host, port) =>
+      info(s"Configuring endpoint as $protocol://$host:$port")
+      setEndpoint(s"$protocol://$host:$port")
   }
 }
